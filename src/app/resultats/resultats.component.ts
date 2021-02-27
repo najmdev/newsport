49cd99ac5d9725c7df2e6ae2696ca393f9c574c8
Abdeslam <abdesslamboutaba@gmail.com>
2021-02-27 23:28:55 +0100

    adding fixtures

--- src/app/resultats/resultats.component.ts
+++ src/app/resultats/resultats.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ResultatsService } from '../services/resultats.service';
 
 @Component({
   selector: 'app-resultats',
@@ -7,9 +8,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResultatsComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private service:ResultatsService) { }
+  public fixtures=[];
+  public fixture : any = {
+    home : "",
+    away : "",
+    home_g:0,
+    away_g:0,
+    st_name:"",
+    date:"",
+    elapsed:0
+  };
   ngOnInit(): void {
+    this.service.getresults().subscribe(
+      (data) => {
+        data=data.data;
+        for(let i=0;i<data.length;i++)
+        {
+          /*if(data[i].countryName === 'Spain' || data[i].countryName === 'Italy' || data[i].countryName === 'France')
+          {*/
+            this.fixture={
+              home:data[i].homeName,
+              away:data[i].awayName,
+              home_g:data[i].team_home_90min_goals,
+              away_g:data[i].team_away_90min_goals,
+              st_name:data[i].venueName,
+              date:data[i].date,
+              elapsed:data[i].elapsed
+            }
+            this.fixtures.push(this.fixture);
+          //}
+        }             //Next callback
+     },
+     (error) => {                              //Error callback
+       console.error('error');
+     }
+       );
   }
 
 }
--- src/app/services/resultats.service.ts
+++ src/app/services/resultats.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
 export class ResultatsService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+  public getresults()
+  {
+    const headers = new HttpHeaders({
+      'x-rapidapi-key': '24f44e5e1amsh2c16115e3ddc3e4p1c4ddejsnfb9848493e18'
+    });
+    return this.http.get<any>('https://elenasport-io1.p.rapidapi.com/v2/inplay',{ headers: headers });
+  }
 }
