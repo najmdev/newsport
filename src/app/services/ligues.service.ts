852e557024e596306ca62fb7884d0b94dd0399ce
aminetam <76522265+aminetam@users.noreply.github.com>
2021-03-02 21:33:13 +0100

    League

--- src/app/services/ligues.service.ts
+++ src/app/services/ligues.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,12 @@ import { Injectable } from '@angular/core';
 })
 export class LiguesService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+  public getresults()
+  {
+    const headers = new HttpHeaders({
+      'x-rapidapi-key': '0357c2473cmshf8a9ff9baad0eaep15e193jsn899b7c46b326'
+    });
+    return this.http.get<any>('https://elenasport-io1.p.rapidapi.com/v2/leagues',{ headers: headers });
+  }
 }
